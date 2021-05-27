97f45ff5a1fa65e93f630cb74157eaaac4b7983f
Enrique <e.calatayudc@alumnos.upm.es>
2021-05-27 23:18:35 +0200

    producer_n_msg

--- src/main/scala/zzz/akka/contextbroker/producer/ContextProducerMain.scala
+++ src/main/scala/zzz/akka/contextbroker/producer/ContextProducerMain.scala
@@ -26,19 +26,21 @@ object ContextProducerMain {
     Behaviors.receiveMessage {
       case ValueResponseMsg(text,_) =>
         ctx.log.info("Got message {}", text)
-        sendValue(text)
+        (0 to 10).foreach{ n=>
+          sendValue(text,n)
+        }
         Behaviors.same
       case _ => Behaviors.same
     }
   }
-  private def sendValue(value: String):Unit = {
+  private def sendValue(value: String,id: Int):Unit = {
         import scala.util.{Failure, Success}
         import scala.concurrent.Future
         import akka.http.scaladsl.client.RequestBuilding.Post
         implicit val system = ActorSystem(Behaviors.empty, "SingleRequest")
         // needed for the future flatMap/onComplete in the end
         implicit val executionContext = system.executionContext
-        val json_response = s"""{ "id": 1, "projectName": "$value", "status": "Stats", "duration": 120}"""
+        val json_response = s"""{ "id": $id, "projectName": "$value", "status": "Stats", "duration": 120}"""
         val responseFuture: Future[HttpResponse] = Http().singleRequest(Post("http://127.0.0.1:8080/jobs", HttpEntity(ContentTypes.`application/json`, json_response)))
 
         responseFuture
